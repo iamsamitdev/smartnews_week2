9a2ffcf1a140901382af3988583b29285b7ae57b
Samit Koyom <samitkoyom@gmail.com>
2021-06-21 00:34:02 +0700

    update day 4

--- lib/Screens/Bottomnavmenu/HomeFragment.dart
+++ lib/Screens/Bottomnavmenu/HomeFragment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_news/Screens/Bottomnavmenu/MenuList.dart';
 import 'package:smart_news/Themes/colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeFragment extends StatefulWidget {
   @override
@@ -8,40 +9,46 @@ class HomeFragment extends StatefulWidget {
 }
 
 class _HomeFragmentState extends State<HomeFragment> {
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Text('Smart News',style: TextStyle(color: textColorSecondary),),
-        actions: [
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text(
+            'Smart News',
+            style: TextStyle(color: textColorSecondary),
+          ),
+          actions: [
             IconButton(
               icon: Icon(Icons.video_call),
               onPressed: () {},
             )
-        ]
-      ),
+          ]),
       drawer: Theme(
-        data: Theme.of(context).copyWith(
-          canvasColor: googleColor
-        ),
+        data: Theme.of(context).copyWith(canvasColor: googleColor),
         child: Drawer(
           child: ListView(
             children: [
-      
               UserAccountsDrawerHeader(
-                accountName: Text('Samit Koyom'), 
+                accountName: Text('Samit Koyom'),
                 accountEmail: Text('samit@email.com'),
                 currentAccountPicture: CircleAvatar(
-                  // backgroundImage: AssetImage('assets/images/avatar.jpg'),
-                ),
-                decoration: BoxDecoration(
-                  color: googleColor
-                ),
+                    // backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                    ),
+                decoration: BoxDecoration(color: googleColor),
               ),
-      
+
               // แบบแรก
               // for (final menu in menus)
               //   ListTile(
@@ -51,78 +58,68 @@ class _HomeFragmentState extends State<HomeFragment> {
               //   ),
 
               MenuList(
-                micon: Icons.login, 
-                mname: 'Login', 
-                onTap: (){
-                  Navigator.pushNamed(context, '/login');
-                }
-              ),
-      
-              MenuList(
-                micon: Icons.new_releases, 
-                mname: 'Lastest news', 
-                onTap: (){ 
-                  Navigator.pushNamed(context, '/lastest_news');
-                }
-              ),
-      
-              MenuList(
-                micon: Icons.category, 
-                mname: 'Category', 
-                onTap: (){ 
-                  Navigator.pushNamed(context, '/category');
-                }
-              ),
+                  micon: Icons.login,
+                  mname: 'Login',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/login');
+                  }),
 
               MenuList(
-                micon: Icons.video_collection, 
-                mname: 'Video Lists', 
-                onTap: (){ 
-                  Navigator.pushNamed(context, '/videolist');
-                }
-              ),
-              
-              MenuList(
-                micon: Icons.info, 
-                mname: 'About', 
-                onTap: (){
-                  Navigator.pushNamed(context, '/about');
-                }
-              ),
+                  micon: Icons.new_releases,
+                  mname: 'Lastest news',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/lastest_news');
+                  }),
 
               MenuList(
-                micon: Icons.settings, 
-                mname: 'Setting', 
-                onTap: (){
-                  Navigator.pushNamed(context, '/setting');
-                }
-              ),
+                  micon: Icons.category,
+                  mname: 'Category',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/category');
+                  }),
 
               MenuList(
-                micon: Icons.logout, 
-                mname: 'Logout', 
-                onTap: (){ print('Logout tapped'); }
-              ),
-              
+                  micon: Icons.video_collection,
+                  mname: 'Video Lists',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/videolist');
+                  }),
+
+              MenuList(
+                  micon: Icons.info,
+                  mname: 'About',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/about');
+                  }),
+
+              MenuList(
+                  micon: Icons.settings,
+                  mname: 'Setting',
+                  onTap: () {
+                    Navigator.popAndPushNamed(context, '/setting');
+                  }),
+
+              MenuList(
+                  micon: Icons.logout,
+                  mname: 'Logout',
+                  onTap: () {
+                    print('Logout tapped');
+                  }),
             ],
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.all(100.0),
-          width: 100.0,
-          height: 100.0,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: googleColor,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top:35.0, left: 20.0,),
-            child: Text('Home', style: TextStyle(fontSize: 24, color: white_color),),
-          )
-        ),
-      ),
+      body: Container(
+          child: CarouselSlider(
+        options: CarouselOptions(),
+        items: imgList
+            .map((item) => Container(
+                  child: Center(
+                      child:
+                          Image.network(item, fit: BoxFit.cover, width: 1000)),
+                ))
+            .toList(),
+      )),
     );
   }
 }
