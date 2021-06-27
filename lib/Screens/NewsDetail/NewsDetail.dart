c0d033e0ccec1e61c081387e21f59c45837f09f7
Samit Koyom <samitkoyom@gmail.com>
2021-06-28 00:11:21 +0700

    update NewsDetail.dart'

--- lib/Screens/NewsDetail/NewsDetail.dart
+++ lib/Screens/NewsDetail/NewsDetail.dart
@@ -1,4 +1,13 @@
+// import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:smart_news/Model/NewsDetailModel.dart';
+import 'package:smart_news/Network/rest_api.dart';
+import 'package:smart_news/Themes/colors.dart';
+import 'package:smart_news/Utils/Widgets.dart';
+import 'package:smart_news/Utils/constant.dart';
 
 class NewsDetail extends StatefulWidget {
 
@@ -7,6 +16,22 @@ class NewsDetail extends StatefulWidget {
 }
 
 class _NewsDetailState extends State<NewsDetail> {
+
+  // เรียกใช้งาน Model
+  NewsDetailModel? newsDetailModel;
+
+  // สร้างฟังก์ชันอ่านรายละเอียดข่าว
+  readNewsDetail(id) async {
+    try{
+      var response = await CallAPI().getNewsDetail(id);
+      setState(() {
+        newsDetailModel = response;
+      });
+    }catch(error){
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -14,10 +39,126 @@ class _NewsDetailState extends State<NewsDetail> {
     final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
 
     // ทดสอบแสดงผล id ที่ได้มา
-    print(arguments['id'].toString());
+    // print(arguments['id'].toString());
 
+    // เรียกฟังก์ชัน readNewsDetail
+    readNewsDetail(arguments['id'].toString());
+    
     return Scaffold(
-      body: Center(child: Text('News Detail'),),
+      body: newsDetailModel != null ? NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: context.height() * 0.55,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Hero(
+                  tag: newsDetailModel!.id!, 
+                  child: cachedImage(newsDetailModel!.embedded!.wpFeaturedmedia![0].sourceUrl.toString(),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                collapseMode: CollapseMode.parallax,
+                title: newsDetailModel != null ?
+                Text(
+                  parseHtmlString(newsDetailModel!.title!.rendered),
+                  style: boldTextStyle(size: textSizeNormal, color: Theme.of(context).textTheme.headline6!.color),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ).visible(innerBoxIsScrolled).paddingOnly(left: isIos ? 16 : 0) : SizedBox(),
+              ),
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              leading: BackButton(
+                color: Colors.black,
+                onPressed: () async {
+                  finish(context);
+                },
+              ),
+            )
+          ];
+        },
+        body: newsDetailModel != null ?
+        Container(
+          color: Theme.of(context).cardTheme.color,
+          height: context.height(),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  10.height,
+                  Text(
+                    '${parseHtmlString(newsDetailModel!.title!.rendered)}',
+                    style: boldTextStyle(
+                      size: textSizeLarge,
+                      color: Theme.of(context).textTheme.headline6!.color,
+                    ),
+                    maxLines: 5,
+                  ),
+                  8.height,
+                  Text(
+                    '${'Author By ${parseHtmlString(newsDetailModel!.embedded!.author![0].name)}'}',
+                    style: primaryTextStyle(color: Colors.blue, size: 14),
+                  ).visible(newsDetailModel!.embedded!.author![0].name!.isNotEmpty),
+                  10.height,
+                  Text(
+                    parseHtmlString(newsDetailModel!.content!.rendered),
+                    style: secondaryTextStyle(size: textSizeSMedium),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ): Padding(
+          padding: const EdgeInsets.only(top:250.0),
+          child: SizedBox(
+            height: 200.0,
+            child: Stack(
+              children: [
+                Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: new CircularProgressIndicator(
+                      strokeWidth: 1,
+                      color: primaryColor,
+                      value: 1.0,
+                    ),
+                  ),
+                ),
+                Center(child: Text("กำลังโหลด...")),
+              ],
+            ),
+          ),
+        ),
+      ) : Padding(
+          padding: const EdgeInsets.only(top:250.0),
+          child: SizedBox(
+            height: 200.0,
+            child: Stack(
+              children: [
+                Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: new CircularProgressIndicator(
+                      strokeWidth: 1,
+                      color: primaryColor,
+                      value: 1.0,
+                    ),
+                  ),
+                ),
+                Center(child: Text("กำลังโหลด...")),
+              ],
+            ),
+          ),
+      ),
     );
   }
+
+  String parseHtmlString(String? htmlString) {
+    return parse(parse(htmlString).body!.text).documentElement!.text;
+  }
+  
 }
